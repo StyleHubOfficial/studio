283b808f71c0a60e63d4d3dae561f53dc5b8954c
Style Hub <stulehubofficial01@gmail.com>
2025-12-03 13:46:58 +0000

    Again showing that error

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,10 +1,8 @@
 
-import { FirebaseClientProvider } from '@/firebase/client-provider';
-
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <FirebaseClientProvider>{children}</FirebaseClientProvider>;
+  return <>{children}</>;
 }

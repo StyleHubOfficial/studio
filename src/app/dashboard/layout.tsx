f6eb37efb6848c2e156742e3a4e56297ede5d5a3
Style Hub <stulehubofficial01@gmail.com>
2025-12-03 13:39:30 +0000

    Sync changes to GitHub

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,10 @@
+
+import { FirebaseClientProvider } from '@/firebase/client-provider';
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <FirebaseClientProvider>{children}</FirebaseClientProvider>;
+}
